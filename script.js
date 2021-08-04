126c4d68ce8efddb7de68e2d5c3bd09ef5745411
Precious Olusola <preciousolusola16@gmail.com>
2021-08-04 20:11:48 +0100

    Calculator is calculating

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Hello World");
+const input = document.querySelector('input');
+const actions = {
+    'is_action' (val) {
+        return ['=', 'RESET', 'DEL'].includes(val)
+    },
+    'is__clear' () {
+        input.value = '';
+    },
+    'is__del' () {
+        input.value = input.value.slice(0, -1);
+    },
+    'is__equal' () {
+        let value = input.value.replace('x', '*');
+        let calculate = eval(value);
+        input.value = calculate;
+    }
+
+}
+
+function CaptureClick(val) {
+    let temp = actions.is_action(val);
+    console.log(temp)
+    if (!temp) {
+        input.value += val;
+        return
+    }
+    if (val === 'RESET') {
+        actions.is__clear();
+        return
+    }
+    if (val === '=') {
+        actions.is__equal();
+        return
+    }
+    if (val === 'DEL') {
+        actions.is__del();
+        return
+    }
+}
+
+const buttons = [...document.getElementsByTagName("button")];
+buttons.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        CaptureClick(this.textContent.trim())
+    })
+})
