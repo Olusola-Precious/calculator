f04058438b36250ec447a5baa22ed3323009f4f9
Precious Olusola <preciousolusola16@gmail.com>
2021-08-05 20:29:19 +0100

    Completed the Project

--- theme.js
+++ theme.js
@@ -6,10 +6,20 @@ const $theme3 = document.getElementById("theme3");
 [$theme1, $theme2, $theme3].forEach(function(each) {
     each.addEventListener('click', function() {
         //console.log(this);
+        document.body.classList.remove("theme1");
         $theme1.classList.add("hidden");
+        document.body.classList.remove("theme2");
         $theme2.classList.add("hidden");
+        document.body.classList.remove("theme3");
         $theme3.classList.add("hidden");
 
+        //document.body.id = "theme6"
+        //console.log(this.id);
+        document.body.classList.add(this.id);
         this.classList.remove("hidden");
+
     })
-})
+});
+
+//$theme2.click();
+//console.log(document.body.classList);
